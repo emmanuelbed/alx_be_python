888b5f91e26a4a7fe03d9ce1e9ce0fb125800c1b
emmanuelbed <bedzoemmanuel8@gmail.com>
2025-07-15 07:21:21 -0500

    Basic Arithmetic Exercise

---
+++ basic_operations.py
@@ -0,0 +1,15 @@
+number1 = 10
+number2 = 5
+
+add = number1 + number2
+diff = number1 - number2
+prod = number1 * number2
+
+
+print("Sum of",  str(number1), "and ", str(number2), "is", str(add))
+print("Difference of",  str(number1), "and ", str(number2), "is", str(diff))
+print("Product of",  str(number1), "and ", str(number2), "is", str(prod))
+
+
+
+
