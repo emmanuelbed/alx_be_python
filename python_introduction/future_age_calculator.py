7035ad5517466546d9c1be5cd322a56e947ec677
emmanuelbed <bedzoemmanuel8@gmail.com>
2025-07-15 07:46:49 -0500

    Add file to 'predict' a usersage

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,7 @@
+#Objective: Practice receiving user input in Python and perform a simple arithmetic operation to calculate the user’s age in a future year.
+
+user_age = input("How old are you? ")
+
+estimaeted_age = int(user_age) + 27
+
+print("In 2050, you will be", str(estimaeted_age), "years old.")
