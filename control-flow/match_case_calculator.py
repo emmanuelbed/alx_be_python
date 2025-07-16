df3ea5c74e4e1529a7800839c715e14f96919039
emmanuelbed <bedzoemmanuel8@gmail.com>
2025-07-16 06:47:54 -0500

    Match case calculator

---
+++ control-flow/match_case_calculator.py
@@ -0,0 +1,23 @@
+# Objective: Learn to use Match Case statements for handling multiple operations in a simple calculator program.
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+operation = input("Choose the operation (+, -, *, /)")
+
+match operation:
+    case "+":
+        result = num1 + num2
+    case "-":
+        result = num1 - num2
+    case "*":
+        result = num1 * num2
+    case "/":
+        if num2 == 0:
+            result = "Cannot divide by zero."
+            print(result)
+            exit()
+        else:
+            result = num1 / num2
+
+print("The result is", result)
