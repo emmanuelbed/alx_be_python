d03ddd32d76e7b852d4420f98594eb39748fce18
emmanuelbed <bedzoemmanuel8@gmail.com>
2025-07-16 04:02:21 -0500

    Change user_input to wether

--- control-flow/weather_advice.py
+++ control-flow/weather_advice.py
@@ -1,14 +1,13 @@
 # Objective: Utilize conditional statements to guide program execution based on user input regarding weather conditions.
 
-user_input = input("What's the weather like today? (sunny/rainy/cold): ").lower()
+weather = input("What's the weather like today? (sunny/rainy/cold): ").lower()
 
-if user_input == "sunny":
+if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
-if user_input == "rainy":
+elif weather == "rainy":
     print("Don't forget your umbrella and a raincoat.")
-if user_input == "cold":
+elif weather == "cold":
     print("Make sure to wear a warm coat and a scarf.")
 else:
     print("Sorry, I don't have recommendations for this weather.")
-
 
