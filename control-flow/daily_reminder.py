af253b07c59ec8a1a5a0dafd1422a6c22896b4a6
emmanuelbed <bedzoemmanuel8@gmail.com>
2025-07-19 05:48:15 -0500

    Priority Task scheduler

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -4,23 +4,20 @@ task = input("Enter your task:")
 priority = input("Priority (high/medium/low):")
 time_bound = input("Is it time-bound? (yes/no):")
 
-match time_bound:
-    case "yes":
-        if priority == "high":
-            print(f"'{task}' is a {priority} priority taskpriority task that requires immediate attention today!")
-        elif priority == "medium":
+match priority:
+    case "high":
+        if time_bound == "yes":
+            print(f"'{task}' is a {priority} priority task that requires immediate attention today!")
+        elif time_bound == "no":
             print(f"'{task}' is a {priority} priority task. Consider completing it very soon.")
-        elif priority == "low":
-            print(f"'{task}' is a {priority} priority task. Consider completing it when you have free time.")
-        else:
-            print("Incorrect Priority level")
-    case "no":
-        if priority == "high":
-            print(f"'{task}' is a {priority} priority taskpriority task that requires immediate attention today!")
-        elif priority == "medium":
+    case "medium":
+        if time_bound == "yes":
+            print(f"'{task}' is a {priority} priority task that requires immediate attention today!")
+        elif time_bound == "no":
             print(f"'{task}' is a {priority} priority task. Consider completing it very soon.")
-        elif priority == "low":
-            print(f"'{task}' is a {priority} priority task. Consider completing it when you have free time.")
-        else:
-            print("Incorrect Priority level")
+    case "low":
+        if time_bound == "yes":
+            print(f"'{task}' is a {priority} priority task that requires immediate attention today!")
+        elif time_bound == "no":
+            print(f"'{task}' is a {priority} priority task. Consider completing it very soon.")    
     
