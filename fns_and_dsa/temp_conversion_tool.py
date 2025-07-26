fd3e09bb04cc26147727686b7377e1fe52414ea4
emmanuelbed <bedzoemmanuel8@gmail.com>
2025-07-26 07:16:48 -0500

    Temperature conversions

---
+++ fns_and_dsa/temp_conversion_tool.py
@@ -0,0 +1,24 @@
+# Objective: Illustrate the concept of variable scope by creating a script that converts temperatures between Celsius and Fahrenheit, using global variables to store conversion factors.
+
+FAHRENHEIT_TO_CELSIUS_FACTOR = (5/9)
+CELSIUS_TO_FAHRENHEIT_FACTOR = (9/5)
+
+user_temp = int(input("Enter the temperature to convert: "))
+conv_type = input("Is this temperature in Celsius or Fahrenheit? (C/F): ").upper()
+
+def convert_to_celsius(fahrenheit):
+    return fahrenheit * FAHRENHEIT_TO_CELSIUS_FACTOR
+
+def convert_to_fahrenheit(celsius):
+    return celsius * CELSIUS_TO_FAHRENHEIT_FACTOR
+
+
+if conv_type ==  "C":
+    value = convert_to_fahrenheit(user_temp)
+    print(user_temp,"°C is", value,"°F")
+elif conv_type == "F":
+    value = convert_to_celsius(user_temp)
+    print(user_temp, "°F is", value, "°C")
+else:
+    print("Sorry, you entered an invalid Type")
+
