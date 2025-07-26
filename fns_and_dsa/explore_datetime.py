ac36136cd96a672c296d2e7272d54b71dd0276b4
emmanuelbed <bedzoemmanuel8@gmail.com>
2025-07-26 06:57:29 -0500

    Adjusted the date

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,8 +1,8 @@
 # Objective: Familiarize yourself with Python’s datetime module by writing a script that performs specified operations with dates and times.
-from datetime import datetime
+from datetime import datetime, date, timedelta
 
 def display_current_datetime():
-    current_date = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print("Current date and time:", current_date)
 
 
@@ -11,8 +11,8 @@ user = int(input("Enter the number of days to add to the current date: "))
 
 
 def calculate_future_date():
-    future_date = datetime.date.today() + datetime.timedelta(days=user)
-    print("Future date:", future_date)
+    future_date = date.today() + timedelta(days=user)
+    print("Future date:", future_date.strftime("%Y-%m-%d"))
 
 
 display_current_datetime()
