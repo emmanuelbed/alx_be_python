c9b72b1cd7b03081aa0273369152762992df7a48
emmanuelbed <bedzoemmanuel8@gmail.com>
2025-07-26 06:04:16 -0500

    Convert numbers at the function's begining

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,6 +1,8 @@
 # Create a Python script named arithmetic_operations.py. In this script, define a function that performs basic arithmetic operations. This function, perform_operation, will be imported and used in a separate main.py script, which we provide.
 
 def perform_operation(num1, num2, operation):
+    num1 = int(num1), num2 = int(num2)
+
     if operation == "divide" and num2 == 0:
         print("Division by zero not possible") 
         exit()
@@ -15,13 +17,13 @@ def perform_operation(num1, num2, operation):
         #     case "divide":
         #         result = int(num1) / int(num2)
         if operation == "add":
-            result = int(num1) + int(num2)
+            result = num1 + num2
         elif operation =="subtract":
-            result = int(num1) - int(num2)
+            result = num1 - num2
         elif operation == "multiply":
-            result = int(num1) * int(num2)
+            result = num1 * num2
         elif operation == "divide":
-            result = int(num1) / int(num2)
+            result = num1 / num2
 
     return result
 
