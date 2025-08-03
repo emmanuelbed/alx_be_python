cae2e9fd7596c4c75177ffa6a9a8402779963e10
emmanuelbed <bedzoemmanuel8@gmail.com>
2025-08-03 16:42:02 -0500

    Now setteled the asert reference Issue

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -11,10 +11,10 @@ class TestCalculator(unittest.TestCase):
     def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5,3), 2)
         
-    def test_multiply(self):
+    def test_multiplication(self):
         self.assertEqual(self.calc.multiply(5,3), 15)
 
-    def test_divide(self):
+    def test_division(self):
         self.assertEqual(self.calc.divide(6,3), 2)
 
     def test_divide_by_zero(self):
