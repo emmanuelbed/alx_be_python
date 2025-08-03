e9dd3ecfe6302d51fefc5ecb6a71eec6f8c3ae36
emmanuelbed <bedzoemmanuel8@gmail.com>
2025-08-03 08:00:35 -0500

    Working with OOP

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,20 @@
+# Objective: Understand the fundamentals of OOP in Python by implementing a BankAccount class that encapsulates banking operations. Use command line arguments to interact with instances of this class.
+
+class BankAccount:
+    def __init__(self, account_balance):
+        self.account_balance = 0
+
+    def deposit(self, amount):
+        self.account_balance += amount
+
+    def withdraw(self, amount):
+        if self.account_balance >= amount:
+            self.account_balance -= amount
+            return True
+        else:
+            return False
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.account_balance}")
+
+
