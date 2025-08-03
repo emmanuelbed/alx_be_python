33390394e7ca2747105b2520c9edde2737f5de30
emmanuelbed <bedzoemmanuel8@gmail.com>
2025-08-03 16:14:25 -0500

    Unit Testing

---
+++ programming_paradigm/test_simple_calculator.py
@@ -0,0 +1,25 @@
+import unittest
+from simple_calculator import SimpleCalculator
+
+class TestCalculator(unittest.TestCase):
+    def test_add(self):
+        result = SimpleCalculator.add(self, 5,3)
+        self.assertEqual(result, 8)
+    def test_subtract(self):
+        result = SimpleCalculator.subtract(self, 5,3)
+        self.assertEqual(result, 2)
+        
+    def test_multiply(self):
+        result = SimpleCalculator.multiply(self, 5,3)
+        self.assertEqual(result, 15)
+
+    def test_divide(self):
+        result = SimpleCalculator.divide(self, 6,3)
+        self.assertEqual(result, 2)
+
+    def test_divide_by_zero(self):
+        result = SimpleCalculator.divide(self, 5, 0)
+        self.assertEqual(result, None)
+
+if __name__ == "__main__":
+    unittest.main()
